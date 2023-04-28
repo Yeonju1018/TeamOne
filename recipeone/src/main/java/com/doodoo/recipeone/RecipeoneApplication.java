e28d2c4f92e897c8c3991fe388503b425969c585
choonsik <juu1018w@gmail.com>
2023-04-28 16:13:36 +0900

    recipeone 첫번째 커밋

---
+++ recipeone/src/main/java/com/doodoo/recipeone/RecipeoneApplication.java
@@ -0,0 +1,15 @@
+package com.doodoo.recipeone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class RecipeoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeoneApplication.class, args);
+	}
+
+
+}
