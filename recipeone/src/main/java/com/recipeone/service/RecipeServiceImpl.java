b086760e73d499654ebbf0363c39a2e3477f448a
CHO HYEONGCHAN <118033064+HYEONGCHANCHO@users.noreply.github.com>
2023-05-17 17:05:17 +0900

    중간 병합

--- recipeone/src/main/java/com/recipeone/service/RecipeServiceImpl.java
+++ recipeone/src/main/java/com/recipeone/service/RecipeServiceImpl.java
@@ -1,6 +1,15 @@
 package com.recipeone.service;
 
+import com.recipeone.dto.RecipeFormDto;
+import com.recipeone.dto.RecipeIngredientDto;
+import com.recipeone.entity.Recipe;
+import com.recipeone.entity.RecipeImg;
+import com.recipeone.entity.RecipeIngredient;
+import com.recipeone.entity.RecipeStep;
+import com.recipeone.repository.RecipeImgRepository;
+import com.recipeone.repository.RecipeRepository;
 import com.recipeone.repository.RecipeSampleRepository;
+import com.recipeone.repository.RecipeStepRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
@@ -8,10 +17,12 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.commons.text.similarity.JaroWinklerDistance;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Log4j2
@@ -20,6 +31,12 @@ import org.apache.commons.text.similarity.JaroWinklerDistance;
 public class RecipeSampleServiceImpl implements RecipeSampleService {
     private final ModelMapper modelMapper;
     private final RecipeSampleRepository recipeSampleRepository;
+    private final RecipeRepository recipeRepository;
+    private final RecipeImgService recipeImgService;
+    private final RecipeImgRepository recipeImgRepository;
+    private final RecipeStepRepository recipeStepRepository;
+    private final RecipeStepService recipeStepService;
+    private RecipeIngredientService recipeIngredientService;
 
     @Override
     public List<Long> searched(String keyword) throws RecipeIdExistException {
@@ -68,6 +85,67 @@ public class RecipeSampleServiceImpl implements RecipeSampleService {
         log.info("recommendedKeywords======"+recommendedKeywords);
         return recommendedKeywords;
     }
+    public Long saveRecipe(RecipeFormDto recipeFormDto, List<MultipartFile> recipeImgFileList) throws Exception {
+
+        // 레시피 등록
+        Recipe recipe = recipeFormDto.createRecipe();
+        recipeRepository.save(recipe);
+
+        // 이미지 등록
+        for (int i=0; i< recipeImgFileList.size(); i++){
+            RecipeImg recipeImg = new RecipeImg();
+            recipeImg.setRecipe(recipe);
+
+            // 대표이미지 설정
+            if (i==0){
+                recipeImg.setRepimgYn("Y");
+            } else {
+                recipeImg.setRepimgYn("N");
+            }
+            recipeImgService.saveRecipeImg(recipeImg, recipeImgFileList.get(i));
+        }
+
+        // 요리순서 등록
+        List<RecipeStep> recipeSteps = new ArrayList<>();
+        for (int j=0; j<recipeImgFileList.size(); j++) {
+            RecipeStep recipeStep = new RecipeStep();
+            recipeStep.setRecipe(recipe);
+            recipeStep.setSteptext("왜 값이 안들어갈까"+j);
+            //Map<String, Object> steptext = new HashMap<String, Object>();
+            //steptext.put(recipeStep.getSteptext(), j);
+
+            recipeSteps.add(recipeStep);
+            //recipeStepService.saveRecipeStep(recipeStep);
+        }
+        recipeStepRepository.saveAll(recipeSteps);
+
+        return recipe.getId();
+    }
+
+
+    public void addIngredientToRecipe(Long recipeId, RecipeIngredientDto recipeIngredientDto) {
+        Recipe recipe = getRecipeById(recipeId);
+
+        // RecipeIngredient 생성 및 설정
+        RecipeIngredient recipeIngredient = new RecipeIngredient();
+        recipeIngredient.createRecipeIngredient(recipeIngredientDto);
+        recipeIngredient.setRecipe(recipe);
+
+        // RecipeIngredient 저장
+        recipeIngredientService.saveRecipeIngredient(recipeIngredient);
+
+        // Recipe의 ingredients 리스트에 추가
+        recipe.getRecipeIngredients().add(recipeIngredient);
+
+        // Recipe 저장
+        recipeRepository.save(recipe);
+
+    }
+    public Recipe getRecipeById(Long recipeId) {
+        return recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new NoSuchElementException("Recipe with ID " + recipeId + " not found"));
+    }
+
 
 
     //    아래는 되는 기본 코드
