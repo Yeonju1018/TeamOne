914dae5380805917411a03e43c4076c2ecf9c07d
choonsik <juu1018w@gmail.com>
2023-06-15 12:35:10 +0900

    기존 파일 삭제

--- recipeone/src/main/java/com/recipeone/repository/MemberPageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recipeone.repository;
-
-import com.recipeone.entity.Memberpagelog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface MemberPageRepository extends JpaRepository<Memberpagelog,Long> {
-
-}
